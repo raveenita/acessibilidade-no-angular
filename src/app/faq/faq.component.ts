691ab462da82bea591a7c33ff38905b24f5a1547
Raveenita <ribeiro.geovana@outlook.com>
2022-01-09 03:02:57 -0300

    refactor(faq): adiciona componente de accordion

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent implements OnInit {
+export class FaqComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    
   }
 
+  ngOnInit(): void {
+  }
 }
--- src/app/faq/faq.module.ts
+++ src/app/faq/faq.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FaqComponent } from './faq.component';
 import { SharedModule } from '../shared/shared.module';
 import { FaqRoutingModule } from './faq-routing.module';
-
-
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,8 @@ import { FaqRoutingModule } from './faq-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    FaqRoutingModule
+    FaqRoutingModule,
+    MatExpansionModule
   ]
 })
 export class FaqModule { }
