e77fb11d26e87a235c63bcb8dbb3e53f5c2e1206
Raveenita <ribeiro.geovana@outlook.com>
2022-02-21 01:24:45 -0300

    refactor: comenta TODO de controle de mídias e animações

--- src/app/shared/components/disable-animation/disable-animation.component.ts
+++ src/app/shared/components/disable-animation/disable-animation.component.ts
@@ -1,27 +1,16 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-disable-animation',
   templateUrl: './disable-animation.component.html',
   styleUrls: ['./disable-animation.component.scss']
 })
-export class DisableAnimationComponent implements OnInit {
-  isAnimationDisabled = false;
+export class DisableAnimationComponent {
 
-  constructor(
-    private renderer: Renderer2
-  ) { }
-
-  public ngOnInit(): void {
-
-  }
+  constructor() { }
 
   public toggleAnimationClass() {
-    if (this.isAnimationDisabled) {
-      this.renderer.addClass(document.body, 'animate');
-    } else {
-      this.renderer.removeClass(document.body, 'animate');
-    }
+    // TODO #4: Alternativa para o usuário controlar mídias e animações
   }
 
 }
