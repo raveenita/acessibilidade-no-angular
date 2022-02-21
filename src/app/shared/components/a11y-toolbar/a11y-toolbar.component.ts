84d43afd73c8ececfbd51767396fc3cffa6d05de
Raveenita <ribeiro.geovana@outlook.com>
2022-02-21 13:57:14 -0300

    refactor: adiciona TODO de skip content e navegação por teclado

--- src/app/shared/components/a11y-toolbar/a11y-toolbar.component.ts
+++ src/app/shared/components/a11y-toolbar/a11y-toolbar.component.ts
@@ -8,31 +8,12 @@ import { filter } from 'rxjs';
   styleUrls: ['./a11y-toolbar.component.scss']
 })
 export class A11yToolbarComponent implements OnInit {
-  public isHighContrastEnabled = false;
   public highContrastAction = 'Habilitar';
-  public skipLinkPath: string | undefined;
 
-  constructor(
-    private renderer: Renderer2,
-    private router: Router
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd))
-      .subscribe(event => {
-        const currentPath = (event as NavigationEnd).url.split('#')[0];
-        this.skipLinkPath = `${currentPath}#main-content`;
-      });
-  }
-
-  private getElementFontSize(element: HTMLElement) {
-    const DEFAULT_MEASURE = 'px';
-    const EMPTY_STRING = '';
-    const currentFontSize = getComputedStyle(element!, null).fontSize;
-    const fontSizeWithoutMeasure = currentFontSize.replace(DEFAULT_MEASURE, EMPTY_STRING);
-
-    return Number(fontSizeWithoutMeasure);
+    // TODO #10: Skip link e navegação por teclado
   }
 
   public increaseFontSize() {
