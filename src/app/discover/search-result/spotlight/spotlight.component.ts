234b1673f68d300f8d7e253d564572ab974e732c
Raveenita <ribeiro.geovana@outlook.com>
2022-02-21 00:08:56 -0300

    refactor: move pasta backend para a raiz

--- src/app/discover/search-result/spotlight/spotlight.component.ts
+++ src/app/discover/search-result/spotlight/spotlight.component.ts
@@ -8,10 +8,9 @@ import { Track } from 'src/app/models/track.interface';
 })
 export class SpotlightComponent implements OnInit {
   @Input() spotlightedTrack: Track | undefined; 
+  
   constructor() { }
 
-  ngOnInit(): void {
-  console.log(this.spotlightedTrack);
-  }
+  ngOnInit(): void {}
 
 }
